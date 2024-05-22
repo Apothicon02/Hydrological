41bc7d7418e2d8817f4174b82f3a368a23818b80
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-05-22 12:08:10 -0400

    - Added tuff around the rims of magma vents. - Made magma vents pointier. - Oceans now have dense seagrass fields ontop of moss.

--- src/main/java/com/Apothic0n/Hydrological/api/biome/features/configurations/VentConfiguration.java
+++ src/main/java/com/Apothic0n/Hydrological/api/biome/features/configurations/VentConfiguration.java
@@ -11,6 +11,8 @@ public class VentConfiguration implements FeatureConfiguration {
     public static final Codec<VentConfiguration> CODEC = RecordCodecBuilder.create((fields) -> {
         return fields.group(BlockStateProvider.CODEC.fieldOf("outer").forGetter((v) -> {
             return v.outer;
+        }), BlockStateProvider.CODEC.fieldOf("rim").forGetter((v) -> {
+            return v.rim;
         }), BlockStateProvider.CODEC.fieldOf("inner").forGetter((v) -> {
             return v.inner;
         }), IntProvider.codec(3, 64).fieldOf("height").forGetter((v) -> {
@@ -23,13 +25,15 @@ public class VentConfiguration implements FeatureConfiguration {
     });
 
     public final BlockStateProvider outer;
+    public final BlockStateProvider rim;
     private final BlockStateProvider inner;
     private final IntProvider height;
     private final IntProvider radius;
     private final IntProvider maxAltitude;
 
-    public VentConfiguration(BlockStateProvider outer, BlockStateProvider inner, IntProvider height, IntProvider radius, IntProvider maxAltitude) {
+    public VentConfiguration(BlockStateProvider outer, BlockStateProvider rim, BlockStateProvider inner, IntProvider height, IntProvider radius, IntProvider maxAltitude) {
         this.outer = outer;
+        this.rim = rim;
         this.inner = inner;
         this.height = height;
         this.radius = radius;
@@ -37,6 +41,7 @@ public class VentConfiguration implements FeatureConfiguration {
     }
 
     public BlockStateProvider getOuter() {return this.outer;}
+    public BlockStateProvider getRim() {return this.rim;}
     public BlockStateProvider getInner() {return this.inner;}
     public IntProvider getHeight() {return this.height;}
     public IntProvider getRadius() {return this.radius;}
--- src/main/java/com/Apothic0n/Hydrological/api/biome/features/types/VentFeature.java
+++ src/main/java/com/Apothic0n/Hydrological/api/biome/features/types/VentFeature.java
@@ -22,18 +22,29 @@ public class VentFeature extends Feature<VentConfiguration> {
         VentConfiguration config = pContext.config();
         int height = config.getHeight().sample(random);
         int radius = config.getRadius().sample(random);
-        int sharpness = height/5;
+        int sharpness = height/6;
         int offset = (int) (10-(radius/1.33));
         BlockPos origin = new BlockPos((int) pContext.origin().getCenter().x() + random.nextInt(-offset, offset), pContext.origin().getY() - 2, (int) pContext.origin().getCenter().z() + random.nextInt(-offset, offset));
         boolean placedAnything = false;
         if (pContext.origin().getY()+height < config.getMaxAltitude().sample(random) && worldGenLevel.getBlockState(origin.north(radius).below(1)).isSolid() && worldGenLevel.getBlockState(origin.east(radius).below(1)).isSolid() &&
                 worldGenLevel.getBlockState(origin.south(radius).below(1)).isSolid() && worldGenLevel.getBlockState(origin.west(radius).below(1)).isSolid()) {
+            int xWarp = 0;
+            int zWarp = 0;
             for (int y = origin.getY(); y <= origin.getY()+height; y++) {
                 BlockState outer = config.getOuter().getState(random, pContext.origin().atY(y));
+                BlockState rim = config.getRim().getState(random, pContext.origin().atY(y));
                 BlockState inner = config.getInner().getState(random, pContext.origin().atY(y));
+                if (random.nextInt(0, 10) > 4+(height/2.5)) {
+                    xWarp += 1;
+                }
+                if (random.nextInt(0, 10) > 4+(height/2.5)) {
+                    zWarp += 1;
+                }
                 for (int x = origin.getX()-radius; x <= origin.getX()+radius; x++) {
                     for (int z = origin.getZ()-radius; z <= origin.getZ()+radius; z++) {
-                        BlockPos pos = new BlockPos(x, y, z);
+                        int effectiveX = x+xWarp;
+                        int effectiveZ = z+zWarp;
+                        BlockPos pos = new BlockPos(effectiveX, y, effectiveZ);
                         int currentHeight = Math.abs(origin.getY()-Math.min(y, origin.getY()+(height-(sharpness))))/(sharpness);
                         int distance = (Math.abs(origin.getX()-x)) + (Math.abs(origin.getZ()-z));
                         if (radius-1 >= distance+currentHeight) {
@@ -41,7 +52,11 @@ public class VentFeature extends Feature<VentConfiguration> {
                                 worldGenLevel.setBlock(pos, inner, UPDATE_ALL);
                             }
                         } else if (radius >= distance+currentHeight) {
-                            worldGenLevel.setBlock(pos, outer, UPDATE_ALL);
+                            if (y == origin.getY()+height || (y+1 == origin.getY()+height && random.nextGaussian() > -0.4) || (y+2 == origin.getY()+height && random.nextGaussian() > 0.5)) {
+                                worldGenLevel.setBlock(pos, rim, UPDATE_ALL);
+                            } else {
+                                worldGenLevel.setBlock(pos, outer, UPDATE_ALL);
+                            }
                         }
                     }
                 }
