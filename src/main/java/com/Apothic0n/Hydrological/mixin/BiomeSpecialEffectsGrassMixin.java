6e790f593809dc2043131f810c45f4720dbdaa2e
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2024-10-24 11:22:36 -0400

    - Made terrain between caves and surface thicker. - Greatly improved cliff biome placement.

--- src/main/java/com/Apothic0n/Hydrological/mixin/BiomeSpecialEffectsGrassMixin.java
+++ src/main/java/com/Apothic0n/Hydrological/mixin/BiomeSpecialEffectsGrassMixin.java
@@ -2,6 +2,7 @@ package com.Apothic0n.Hydrological.mixin;
 
 import com.Apothic0n.Hydrological.api.HydrolDensityFunctions;
 import com.Apothic0n.Hydrological.api.HydrolColorHelper;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.DensityFunction;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,8 +19,8 @@ public class BiomeSpecialEffectsGrassMixin {
      */
     @Inject(at = @At("RETURN"), method = "modifyColor", cancellable = true)
     private void modifyColor(double x, double z, int unusedColor, CallbackInfoReturnable<Integer> cir) {
-        if (HydrolDensityFunctions.temperature != null) {
-            cir.setReturnValue(HydrolColorHelper.tintFoliageOrGrass((int) x, 0, (int) z, HydrolDensityFunctions.temperature.compute(new DensityFunction.SinglePointContext((int) x, 0, (int) z))));
+        if (HydrolDensityFunctions.precomputedMaps.get("temperature") != null && x > 0 && x < HydrolDensityFunctions.worldSize && z > 0 && z < HydrolDensityFunctions.worldSize) {
+            cir.setReturnValue(HydrolColorHelper.tintFoliageOrGrass((int) x, 0, (int) z, HydrolDensityFunctions.precomputedMaps.get("temperature").get(ChunkPos.asLong((int) x, (int) z))));
         }
     }
 }
